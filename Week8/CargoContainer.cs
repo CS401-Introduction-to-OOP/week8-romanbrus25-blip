9e93ebe90905cccb09ff7e10d1c16cc9418e2b56
bruro <roman.brus.25@kse.org.ua>
2026-03-24 18:50:34 +0200

    Finished the second part

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace Week8;
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double totalcost = 0;
+        foreach (T item in _items)
+        {
+            totalcost += item.CalculateCost();
+        }
+
+        return totalcost;
+    }
+}
