7e05c8520124d0a84a14d5d8e25b15a5633fafb8
bruro <roman.brus.25@kse.org.ua>
2026-03-24 18:59:40 +0200

    Finished the third part

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        Letter letter1 = new Letter("1", 1);
+        Letter letter2 = new Letter("2", 0.5);
+        Parcel parcel1 = new Parcel("3", 15, "20*25*20");
+        Parcel parcel2 = new Parcel("4", 25, "20*25*30");
         
+        letter1.PrintInfo();
+        parcel2.PrintInfo();
+        
+        CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+        
+        myCargo.AddItem(letter1);
+        myCargo.AddItem(letter2);
+        myCargo.AddItem(parcel1);
+        myCargo.AddItem(parcel2);
+        
+        Console.WriteLine(myCargo.GetTotalCost());
     }
 }
